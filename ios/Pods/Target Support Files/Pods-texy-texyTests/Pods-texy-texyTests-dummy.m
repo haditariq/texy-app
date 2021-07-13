fe8032f9e7919c85723a00b2979d99e63d79038c
Hadi Tariq <hadi-tariq@live.co.uk>
2021-07-13 20:25:30 +0500

    inital project

---
+++ ios/Pods/Target Support Files/Pods-texy-texyTests/Pods-texy-texyTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_texy_texyTests : NSObject
+@end
+@implementation PodsDummy_Pods_texy_texyTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-texy/Pods-texy-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_texy : NSObject
+@end
+@implementation PodsDummy_Pods_texy
+@end
